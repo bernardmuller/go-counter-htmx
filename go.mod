a11fd033472325455835f0e62be2165c7aaf0c1e
Bernard Muller <b.mullerjnr@gmail.com>
2023-09-06 10:46:17 +0200

    feat: counter htmx

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bernardmuller/go_do_stuff
 
 go 1.21.0
+
+require github.com/julienschmidt/httprouter v1.3.0
--- main.go
+++ main.go
@@ -1,7 +1,87 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"text/template"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+type Counter struct {
+	Value int `json:"counter"`
+}
+
+var counter = Counter{}
+
+func (c *Counter) Increase() {
+	c.Value++
+}
+
+func (c *Counter) Decrease() {
+	c.Value--
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getCounterApi(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(counter)
+}
+
+func IncreaseCounterApi(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	counter.Increase()
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(counter)
+}
+
+func DecreaseCounterApi(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	counter.Decrease()
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(counter)
+}
+
+func getIndex(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	templ, _ := template.ParseFiles("./index.html")
+	data := map[string]int{
+		"Counter": counter.Value,
+	}
+	templ.ExecuteTemplate(w, "index.html", data)
+}
+
+func IncreaseCounter(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	tmplStr := "<div id=\"counter\">{{.Counter}}</div>"
+	tmpl, _ := template.New("counter").Parse(tmplStr)
+	counter.Increase()
+	data := map[string]int{
+		"Counter": counter.Value,
+	}
+	tmpl.ExecuteTemplate(w, "counter", data)
+}
+
+func DecreaseCounter(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	tmplStr := "<div id=\"counter\">{{.Counter}}</div>"
+	tmpl, _ := template.New("counter").Parse(tmplStr)
+	counter.Decrease()
+	data := map[string]int{
+		"Counter": counter.Value,
+	}
+	tmpl.ExecuteTemplate(w, "counter", data)
+}
 
 func main() {
-	fmt.Println("Go Do Stuff!")
+	router := httprouter.New()
+	router.GET("/api/", getCounterApi)
+	router.POST("/api/increase", IncreaseCounterApi)
+	router.POST("/api/decrease", DecreaseCounterApi)
+	router.GET("/", getIndex)
+	router.POST("/increase", IncreaseCounter)
+	router.POST("/decrease", DecreaseCounter)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
